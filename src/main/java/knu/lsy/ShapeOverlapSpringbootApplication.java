b756c3c66b198b04cc1a4763390a24dcf75fb8af
HEO7777 <a23562399@gmail.com>
2025-05-25 14:07:15 +0900

    chore: set up Spring Boot project structure and configuration files

---
+++ src/main/java/knu/lsy/ShapeOverlapSpringbootApplication.java
@@ -0,0 +1,13 @@
+package knu.lsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeOverlapSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShapeOverlapSpringbootApplication.class, args);
+	}
+
+}
